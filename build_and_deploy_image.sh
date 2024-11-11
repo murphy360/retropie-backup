d6f15281c54ae017476c86a1ba95d9dbada120e5
Corey Murphy <c.murphy360@gmail.com>
2024-11-11 11:38:41 -0800

    Improve build and deploy to work with existing docker compose on system.

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -6,6 +6,14 @@ printf "Pulling the latest changes from the repository...\n"
 printf "***************************************************\n\n\n"
 git pull
 
+# Remove existing Retropie Backup Docker containers and images launched in separate docker compose files
+printf "\n\n\n***************************************************\n"
+printf "Removing existing Retropie Backup Docker containers and images...\n"
+printf "***************************************************\n\n\n"
+docker stop $(docker ps -a | grep retropie_backup | awk '{print $1}')
+docker rm $(docker ps -a | grep retropie_backup | awk '{print $1}')
+docker rmi $(docker images | grep retropie_backup | awk '{print $3}')
+
 # Build the Docker image
 printf "\n\n\n***************************************************\n"
 printf "Building the Docker image...\n"
